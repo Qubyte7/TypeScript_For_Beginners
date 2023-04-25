fa2ca46c597a54647c68b8edb8233420146ff55a
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-25 16:29:01 +0200

    array with mutiple datatype

--- myUnio.ts
+++ myUnio.ts
@@ -11,11 +11,19 @@ type Admin = {
     username:string,
     id:number
 }
-
+//managing 2 different data type of an element
 let Innocent: User | Admin ={name:"Innocent",id:1}
 Innocent = {username:"Shami",id:2}
 
-
+function getDbId(id:number | string){
+    if(typeof id === "string"){
+        id.toLowerCase()
+    }else{
+        id+2;
+    }
+}
+//Alertering an array of number and string
+const data:(string | number )[]= [2,"ass"]
 
 
 

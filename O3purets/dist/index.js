12d0b54d9515954ac8f57511d0f8c0ac83c668b1
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-27 21:11:31 +0200

    classes and sub classes

--- O3purets/dist/index.js
+++ O3purets/dist/index.js
@@ -1,11 +1,49 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//CLASSES
+//CLASSES 1st
 class User {
     constructor(email, name) {
         this.city = "huye"; //Tomean that city is not accessible outside of the class
         this.email = email;
         this.name = name;
+        this.behavior = function behavior() {
+            console.log("My email is " + this.email + "Ka my name kange nako ni" + this.name);
+        };
+    }
+}
+//CLASSES 2nd
+class House {
+    constructor(paints, bricks, Chair) {
+        this.paints = paints;
+        this.bricks = bricks;
+        this.Chair = Chair;
+    }
+    //GETTER
+    get getPaints() {
+        return `this are some of the paints we have ${this.paints}`;
+    }
+    get ChairSeat() {
+        return this.Chair;
+    }
+    //SETTER
+    set ChairSeat(ChairNum) {
+        if (ChairNum <= 1) {
+            throw new Error("Chair umber should be mora than One");
+        }
+        this.Chair = ChairNum;
+    }
+    //privet method
+    deleteToken() {
+        console.log("Web token deleted");
     }
 }
 const innocent = new User("innocent@gmail.com", "shami Innocent");
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changePassword() {
+        this.password = "innocent";
+    }
+}
--- O3purets/src/index.ts
+++ O3purets/src/index.ts
@@ -1,18 +1,51 @@
 import { name } from "typescipt";
 
-    //CLASSES
+    //CLASSES 1st
 class User {
+    behavior?:{}
+    protected password?:string//gets private in this class and to any class that extended this class
     email:string
     name:string 
    private readonly  city ="huye"//Tomean that city is not accessible outside of the class
     constructor(email:string,name:string){
         this.email = email;
         this.name = name
+        this.behavior = function behavior(){
+            console.log("My email is "+this.email+"Ka my name kange nako ni"+this.name)
+        }
+    }}
+       //CLASSES 2nd
+       class House {
+        constructor(public paints:string,public bricks:string,private  Chair:number){
+        }
+             //GETTER
+        get getPaints():string{
+            return `this are some of the paints we have ${this.paints}`
+        }
+        get ChairSeat():number{
+            return this.Chair
+        }
+                   //SETTER
+        set ChairSeat(ChairNum){//no datatype return specified
+         if(ChairNum <=1){
+            throw new Error("Chair umber should be mora than One")
+         }
+         this.Chair = ChairNum
+        }
+               //privet method
+        private deleteToken():void{
+            console.log("Web token deleted")
+        }
     }
-}
+
 const innocent = new User("innocent@gmail.com","shami Innocent")
 
-
+class SubUser extends User{
+    isFamily:boolean =  true
+    changePassword (){
+     this.password = "innocent"
+   }
+}
 
 
 

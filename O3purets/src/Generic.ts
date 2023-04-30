2105732b4a3aa8fa42fd621227b8a8eb9c93e550
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-30 19:48:37 +0200

    Generic2

--- O3purets/src/Generic.ts
+++ O3purets/src/Generic.ts
@@ -22,6 +22,62 @@ interface Bootle{
 identity3<Bootle>({brand:"sdi",type:3})
 
 
+function getSearchProducts<T>(products: T[]):T{//here  we have to return 1  value in tha passed argument
+    
+    return products[1]
+}
+
+function DealingWithArray<T>(ARG:Array<T>):Array<T>{
+    console.log(ARG.length);//Arry have arguments so no ARGUMENTS
+    return ARG;
+}
+
+const getMoreSearchProducts = <T,>(products: T[]):T =>{
+        const index = 4
+        return products[index]
+}
+//<T,> the comer is for mentioning that it is a generic 
+
+//ADVANCED USE GENERIC
+//(1)
+function anotherFunction<T, U>(valOne:T, valTwo:U):object {
+    return{
+        valOne,
+        valTwo
+    }
+}
+anotherFunction(1,"free")
+//(2)
+function anotherFunction2<T, U extends number>(valOne:T, valTwo:U):object{
+    return{valOne,valTwo}
+}
+anotherFunction2("innocent",23)
+
+//(3)
+interface genericInterface{
+    name:string,
+    children:number
+}
+function anotherFunction3<T, U extends genericInterface>(valOne:T, valTwo:U):object{
+    return{valOne,valTwo}
+}
+anotherFunction3(12,{name:"Innocent",children:12})
+//GENERIC CLASSES
+class sellable<T>{
+    public cart:T[]=[]
+
+    addToCart(product:T){
+        this.cart.push(product)
+    }
+}
+
+
+
+
+
+
+
+
 
 
 

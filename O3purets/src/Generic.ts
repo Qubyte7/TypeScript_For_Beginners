12e4e40457d229b37cf855a28978f2ef90a4481f
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-30 19:11:06 +0200

    GENERIC

--- O3purets/src/Generic.ts
+++ O3purets/src/Generic.ts
@@ -1,11 +1,25 @@
 const score: Array<number> = []
 
 
+function identity(val:any):any{
+    return val
+}
 
+function identity2<Type>(val:Type):Type{
+    return val
+}
+//Most ways this is written
+function identity3<t>(val:t):t{
+    return val
+}
+identity2(2)//this will return 2
 
-
-
-
+//you can pass an interface in the generic 
+interface Bootle{
+  brand:string,
+  type:number
+}
+identity3<Bootle>({brand:"sdi",type:3})
 
 
 
